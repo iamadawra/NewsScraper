d19733d846e4edfe999a44fb57c82d40b4f8c0c5
Aayush Dawra <aayushdawra@Aayushs-MacBook-Air.local>
2013-06-20 18:22:44 -0700

    Installed Dependencies Mechanize and BeautifulSoup

--- beautifulsoup/README.txt
+++ beautifulsoup/README.txt
@@ -1,7 +1,7 @@
 = Introduction =
 
   >>> from bs4 import BeautifulSoup
-  >>> soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+  >>> 	
   >>> print soup.prettify()
   <html>
    <body>
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # NewsScraper 
 # A Python Script that scrapes the web for Headlines and reads them out loud
 # Author: Aayush Dawra
+
+from bs4 import BeautifulSoup
+import mechanize
