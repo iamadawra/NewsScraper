3fb0ec319373c95a5da327ec908d76089b246223
iamadawra <aayushgopaldawra@gmail.com>
2013-06-20 18:26:42 -0700

    Trying to link to username

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # A Python Script that scrapes the web for Headlines and reads them out loud
 # Author: Aayush Dawra
 
+
+#Dependencies installed and ready
 from bs4 import BeautifulSoup
 import mechanize
 
